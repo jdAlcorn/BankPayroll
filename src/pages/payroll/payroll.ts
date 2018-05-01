c19ce5ae3165b47d845541a16a02eb9bcd9d942d
Eniela Vela <ynnenu@icloud.com>
2018-05-01 18:25:29 -0400

    Revert "Merge branch 'dev' of https://github.com/Soundburst/BankPayroll into dev"
    This reverts commit 0d9daa3315d9d51750710c55b76cabd7e9e91c4f, reversing
    changes made to 7e0038e26599d756f1bb79d2e8c5d980b2b201d8.

--- src/pages/payroll/payroll.ts
+++ src/pages/payroll/payroll.ts
@@ -4,7 +4,6 @@ import {Bridge} from "../../providers/bridge";
 import {Events} from "ionic-angular";
 import {HttpErrorResponse} from "@angular/common/http";
 import { AlertController } from 'ionic-angular';
-
 import * as moment from 'moment';
 /**
  * Generated class for the TestPage page.
@@ -18,112 +17,67 @@ import * as moment from 'moment';
   templateUrl: 'payroll.html',
 })
 
-
 export class PayrollPage {
 
-  // Instance of the currently selected companies data
+ employees = null;
+ companies = null;
+ numComps  = null;
+ selected  = false;
+ companyID = null;
+ clicked = false;
+ coms = null;
 
-  companies = null;
-  numComps  = null;
-
-  selectedCompany = null;
-
-  currentCompany = null;
-  employees = null;
-  payrollHistory = null;
-
-
-  bridge = null;
-
-  private updateCompany(): void {
-    // Reset the previous company data variables
-    this.currentCompany = null;
-    this.employees = null;
-    this.payrollHistory = null;
-
-    // Fetch data for the new company
-    this.getCompany( this.selectedCompany );
-    this.getEmployees(this.selectedCompany);
-    this.getPayrollHistory(this.selectedCompany);
-   }
-
-   private getLastPayPeriofd(){
-     let currentStart = moment(this.currentCompany['payPeriodStart'], "mm/DD/yyyy");
-     let lastStart  = null;
-     let payTime = this.currentCompany['payType'];
-
-     if(payType == "WEEKLY"){
-         lastStart = currentStart.remove(7, 'days');
-     } else if(payType == "BIWEEKLY"){
-         lastStart = currentStart.remove(14, 'days');
-     } else if(payType == "BIMONTHLY"){
-         let dayStart = currentStart.format('D');
-         //16 - 15
-         if(dayStart == 16) lastStart = currentStart.remove(15, 'days');
-         //1 -> 16
-         if(dayStart == 1) lastStart = currentStart.remove(1, 'month').add(14, 'days');
-     }
-     return lastStart;
-   }
+ currentCompany = null;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, bridge: Bridge) {
-    this.bridge = bridge;
 
-      bridge.getCompanies().subscribe (
-        (comps) => {
-          this.numComps = comps.length;
-          this.companies = comps;
-        }
-      )
+ bridge = null;
+
+private stuff(): void {
+  let company = this.coms;
+  this.getCurrentCompany(company);
+  this.getEmployees(company);
+ }
+
+
+private getCurrentCompany(id){
+  for(let company of this.companies){
+    if(company['uID'] == id){
+      this.currentCompany = company;
+      return;
     }
-
-    private getPayrollHistory(companyId){
-      this.bridge.getCompanyPayrollHistory(companyId).subscribe (
-         result => {
-           this.payrollHistory = result;
-         },
-        ( err: HttpErrorResponse ) => {
-          if( err.status == 401 ) // Login credentials rejected
-            console.log("Access denied");
-          else // Some other error
-            console.log("An error has occurred: " + err.statusText);
-        }
-      )
-    }
-
-    private getCompany(companyId){
-      this.bridge.getCompanies().subscribe(
-        result => {
-          for( let company of result ){
-            if( company.uID == companyId ){
-              this.currentCompany = company;
-              break;
-            }
-          }
-        },
-        ( err: HttpErrorResponse ) => {
-          if( err.status == 401 ) // Login credentials rejected
-            console.log("Access denied");
-          else // Some other error
-            console.log("An error has occurred: " + err.statusText);
-        }
-      )
-    }
-
-    private getEmployees(companyId){
-     this.bridge.getEmployees(companyId).subscribe(
-        ( result ) => {
-          // Credentials accepted, user has been authenticated
-          this.employees = result;
-        },
-        ( err: HttpErrorResponse ) => {
-          if( err.status == 401 ) // Login credentials rejected
-            console.log("Access denied");
-          else // Some other error
-            console.log("An error has occurred: " + err.statusText);
-        }
-      )
   }
+}
+
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, bridge: Bridge, private alertCtrl: AlertController) {
+
+   this.bridge = bridge;
+
+ 	 console.log("Getting company data....");
+    bridge.getCompanies().subscribe(
+      (comps) => {
+        this.numComps = comps.length;
+        this.companies = comps;
+          }
+        )
+    }
+
+    private getEmployees(id){
+     this.bridge.getEmployees(id).subscribe(
+          ( result ) => {
+            // Credentials accepted, user has been authenticated
+            this.employees = result;
+            console.log(this.employees);
+          },
+          ( err: HttpErrorResponse ) => {
+            if( err.status == 401 ) // Login credentials rejected
+              console.log("Access denied");
+            else // Some other error
+              console.log("An error has occurred: " + err.statusText);
+          }
+        )
+}
 
 }
