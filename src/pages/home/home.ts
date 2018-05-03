5aeddf83572727471c6474af1a06d49cb7a22ba1
Kyle Goodale <KGoodale13@users.noreply.github.com>
2018-05-03 12:57:04 -0400

    Not pretty company list on home page. Someone plz make it look pretty

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { NavController } from 'ionic-angular';
 import {Bridge} from "../../providers/bridge";
 
 import {EmployeePage} from "../employee/employee";
+import {AuthService} from "../../providers/auth-service";
+import * as moment from "moment";
 
 @Component({
   selector: 'page-home',
@@ -11,18 +13,23 @@ import {EmployeePage} from "../employee/employee";
 export class HomePage {
 	//All links for every pages nav bars
 	employee = EmployeePage;
+  currentUser = null;
+  companies = null;
+  payrollDue = [];
 
-  constructor(public navCtrl: NavController, public bridge: Bridge) {
-
+  constructor(public navCtrl: NavController, public bridge: Bridge, authService: AuthService) {
+    this.currentUser = authService.currentUser;
   }
   ionViewDidLoad(){
 	this.getInfo();
 }
 
 getInfo(){
-	this.bridge.getCompanies();
- 	console.log(this.bridge.companies);
- 	//console.log(this.bridge);
- // alert("Why no work");
+  this.bridge.getCompanies().subscribe (
+    (comps) => {
+      this.companies = comps;
+
+    }
+  );
 }
 }
